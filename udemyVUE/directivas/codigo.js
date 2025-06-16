04776ba67a2e73317cefef3f184708182089c3a5
Alex <alexnicolashoyos@gmail.com>
2025-06-16 16:57:38 -0300

    Agregado v-for

--- udemyVUE/directivas/codigo.js
+++ udemyVUE/directivas/codigo.js
@@ -3,5 +3,11 @@ const app = new Vue({
   data:{
     conectado: true,
     edad: 18,
+    lista:['Hola', 'Mundo', 'Vue.js'],
+    listapersonas:[
+      {nombre: 'Juan', edad: 25},
+      {nombre: 'Ana', edad: 30},
+      {nombre: 'Luis', edad: 22}
+    ]
   }
 })
