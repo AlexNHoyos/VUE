ce1dd1cb8fbe34029db874243e3cf2bb00109e5e
Alex <alexnicolashoyos@gmail.com>
2025-06-21 21:05:13 -0300

    Uso de computed

--- udemyVUE/Esencial/main.js
+++ udemyVUE/Esencial/main.js
@@ -2,11 +2,29 @@ const app = new Vue({
     el: '#main',
     data: {
         tarea: null,
+        tareas: [
+            {titulo: 'Aprender Vue', completado: true},
+            {titulo: 'Aprender ES6', completado: false},
+            {titulo: 'Aprender Java', completado: false},
+            {titulo: 'Aprender C', completado: true},
+            {titulo: 'Aprender C++', completado: true}
+        ],
         tarea_lista:['Hola', 'Mundo', 'Vue'],
     },
     methods: {
         agregarTarea(){
             this.tarea_lista.unshift(this.tarea)
+        },
+        agregarTarea2(){
+            this.tareas.unshift({
+                titulo: this.tarea,
+                completado: false
+            });
         }
-    }
+    }, 
+    computed: {
+        tareasCompletadas(){
+            return this.tareas.filter(tarea => tarea.completado);
+        },
+    },
 })
